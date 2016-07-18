9503ef865903b0a83815ab684a4003320cc311d1
Andres Alvidrez <aalvidrezg@uach.mx>
2016-07-18 12:34:18 -0600

    Se agrega configuracion de smtp

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,8 +86,10 @@ Rails.application.configure do
 
   # Devise default url options.
   config.action_mailer.default_url_options = { host: 'intense-stream-93294.herokuapp.com' }
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default charset: 'utf-8'
 
   config.action_mailer.smtp_settings = {
       address: 'smtp.gmail.com',
